410561e38cb1e384cfbaac25461ddf0c9741906f
jake <47467532+mutantfrogs@users.noreply.github.com>
2023-04-04 14:54:50 -0400

    Add files via upload

---
+++ directory.js
@@ -0,0 +1,55 @@
+// array of objects that is referenced when creating html
+// objects are individual artworks and associated information
+
+// object breakdown:
+// id: unique identifyer
+// url: file location of full artwork
+// thumburl: file location of artwork thumbnail for buttons
+// year: tag for year artwork was made in
+// nsfw: tag for nsfw artwork 
+// future tags? attachments, description, alt.
+
+const portfolio = [
+    //{id: "", url: "", thumburl: "", year: xxxx, nsfw: false},
+    {id: "911", url: "assets2/911.png", thumburl: "assets2/thumbnails/911.png", year: 2022, nsfw: true},
+    {id: "alien", url: "assets2/alien.png", thumburl: "assets2/thumbnails/alien.png", year: 2021, nsfw: false},
+    {id: "ankha", url: "assets2/ankha.png", thumburl: "assets2/thumbnails/", year: 2019, nsfw: false},
+    {id: "apple", url: "assets2/apple.png", thumburl: "assets2/thumbnails/apple.png", year: 2019, nsfw: false},
+    {id: "arm", url: "assets2/arm.png", thumburl: "assets2/thumbnails/arm.png", year: 2019, nsfw: false},
+    {id: "bazel", url: "assets2/bazel.png", thumburl: "assets2/thumbnails/bazel.png", year: 2019, nsfw: true},
+    {id: "beach", url: "assets2/beach.png", thumburl: "assets2/thumbnails/beach.png", year: 2019, nsfw: false},
+    {id: "boots", url: "assets2/boots.png", thumburl: "assets2/thumbnails/boots.png", year: 2019, nsfw: false},
+    {id: "candleman", url: "assets2/candleman.png", thumburl: "assets2/thumbnails/candleman.png", year: 2019, nsfw: false},
+    {id: "charge", url: "assets2/charge.png", thumburl: "assets2/thumbnails/charge.png", year: 2019, nsfw: false},
+    {id: "clown", url: "assets2/clown.png", thumburl: "assets2/thumbnails/.png", year: 2019, nsfw: false},
+    {id: "clownms", url: "assets2/clownms.png", thumburl: "assets2/thumbnails/clownms.png", year: 2019, nsfw: false},
+    {id: "crab", url: "assets2/crab.png", thumburl: "assets2/thumbnails/crab.png", year: 2019, nsfw: false},
+    {id: "critic", url: "assets2/critic.png", thumburl: "assets2/thumbnails/critic.png", year: 2019, nsfw: false},
+    {id: "dioxideDTIYS", url: "assets2/dioxideDTIYS.png", thumburl: "assets2/thumbnails/dioxideDTIYS.png", year: 2023, nsfw: false},
+    {id: "dog", url: "assets2/dog.png", thumburl: "assets2/thumbnails/dog.png", year: 2019, nsfw: false},
+    {id: "dream", url: "assets2/dream.png", thumburl: "assets2/thumbnails/dream.png", year: 2019, nsfw: false},
+    {id: "elephant", url: "assets2/elephant.png", thumburl: "assets2/thumbnails/elephant.png", year: 2019, nsfw: false},
+    {id: "godzilla", url: "assets2/godzilla.png", thumburl: "assets2/thumbnails/godzilla.png", year: 2019, nsfw: false},
+    {id: "herja", url: "assets2/herja.png", thumburl: "assets2/thumbnails/herja.png", year: 2019, nsfw: false},
+    {id: "joker", url: "assets2/joker.png", thumburl: "assets2/thumbnails/joker.png", year: 2019, nsfw: false},
+    {id: "jungle", url: "assets2/jungle.png", thumburl: "assets2/thumbnails/jungle.png", year: 2019, nsfw: false},
+    {id: "marketing1", url: "assets2/marketing1.png", thumburl: "assets2/thumbnails/marketing1.png", year: 2019, nsfw: false},
+    {id: "marketing2", url: "assets2/marketing2.png", thumburl: "assets2/thumbnails/marketing2.png", year: 2019, nsfw: false},
+    {id: "mary", url: "assets2/mary.png", thumburl: "assets2/thumbnails/mary.png", year: 2019, nsfw: false},
+    {id: "monster", url: "assets2/monster.png", thumburl: "assets2/thumbnails/monster.png", year: 2019, nsfw: false},
+    {id: "mspaint", url: "assets2/mspaint.png", thumburl: "assets2/thumbnails/mspaint.png", year: 2019, nsfw: false},
+    {id: "mspaint2", url: "assets2/mspaint2.png", thumburl: "assets2/thumbnails/mspaint.png", year: 2019, nsfw: false},
+    {id: "nether", url: "assets2/nether.png", thumburl: "assets2/thumbnails/nether.png", year: 2019, nsfw: false},
+    {id: "puppet", url: "assets2/puppet.png", thumburl: "assets2/thumbnails/puppet.png", year: 2019, nsfw: false},
+    {id: "shower", url: "assets2/shower.png", thumburl: "assets2/thumbnails/shower.png", year: 2019, nsfw: false},
+    {id: "sky", url: "assets2/sky.png", thumburl: "assets2/thumbnails/sky.png", year: 2019, nsfw: false},
+    {id: "slug", url: "assets2/slug.png", thumburl: "assets2/thumbnails/slug.png", year: 2019, nsfw: false},
+    {id: "tammy", url: "assets2/tammy.png", thumburl: "assets2/thumbnails/tammy.png", year: 2019, nsfw: false},
+    {id: "toaster", url: "assets2/toaster.png", thumburl: "assets2/thumbnails/toaster.png", year: 2019, nsfw: false},
+    {id: "toolz", url: "assets2/toolz.png", thumburl: "assets2/thumbnails/toolz.png", year: 2019, nsfw: false},
+    {id: "troll", url: "assets2/troll.png", thumburl: "assets2/thumbnails/troll.png", year: 2019, nsfw: false},
+    {id: "vent", url: "assets2/vent.png", thumburl: "assets2/thumbnails/vent.png", year: 2019, nsfw: false},
+    {id: "watcher", url: "assets2/watcher.png", thumburl: "assets2/thumbnails/watcher.png", year: 2019, nsfw: false},
+    {id: "woods", url: "assets2/woods.png", thumburl: "assets2/thumbnails/woods.png", year: 2019, nsfw: false},
+    {id: "xphill", url: "assets2/xphill.png", thumburl: "assets2/thumbnails/xphill.png", year: 2019, nsfw: false}
+];
